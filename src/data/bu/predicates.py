6ba6dab2ccd854f3cec8f0fafa3359fad16cf4ac
Cristian-Ioan Vasile <cvasile@bu.edu>
2016-05-26 09:32:38 -0400

    Created a python module with mission specific predicates for the bu campus example.

---
+++ src/data/bu/predicates.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+license_text='''
+    Module implements GDTL-FIRM algorithm.
+    Copyright (C) 2016  Cristian Ioan Vasile <cvasile@bu.edu>
+    Hybrid and Networked Systems (HyNeSs) Group, BU Robotics Lab, Boston University
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+'''
+Created on May 26, 2016
+
+@author: cristi
+'''
+
+def obstacles():
+    return 
+
+predicates = {
+    'Obs': obstacles
+}
+
+
+if __name__ == '__main__':
+    pass

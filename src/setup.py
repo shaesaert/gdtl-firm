b465a2e8670975a045d9013cfdac212b0dcacc93
shaesaert <s.haesaert@tue.nl>
2019-09-09 13:39:02 +0200

    added setup file

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='gdtl-firm',
+    version='0.1',
+    packages=['firm','gdtl','data'],
+    license='GNU',
+    author='Cristian Ioan Vasile',
+    author_email='cvasile@bu.edu',
+    description='The project implements motion planning for uncertain systems with rich specifications that capture both temporal and uncertainty constraints.'
+)
